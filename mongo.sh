24e9b5364cb7ef7d215e7994fbb1d9508760ffc7
pstet <123316024+pcs03@users.noreply.github.com>
2024-09-12 17:40:50 +0200

    Update mongo.sh script to accomodate for runnning mongo instance

--- mongo.sh
+++ mongo.sh
@@ -1,1 +1,7 @@
-docker run -d --name mongo -p "27017:27017" mongo
+if [[ -z $(docker ps -a | grep mongo) ]]; then
+    docker run -d --name mongo -p "27017:27017" mongo
+else
+    docker start mongo
+fi
+
+
